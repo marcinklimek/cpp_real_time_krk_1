2414b449358db564a8135cf0985950310bed274a
Marcin Klimek <git@marcinklimek.pl>
2014-02-25 10:52:39 +0100

    fork

--- day2/fork/fork_simple.cpp
+++ day2/fork/fork_simple.cpp
@@ -2,19 +2,46 @@
 #include <stdio.h>
 #include <iostream>
 #include "unistd.h"
+#include "pid_info.h"
 
 using namespace std;
 
 int main()
 {
+    pid_t pid;
     cout << " A " << endl;
-
-    fork();  cout.flush();
-    cout << " B" << endl; 
     cout.flush();
+    //pid_info("A");
 
-    fork();
-    cout << " C " << endl;
-    cout.flush();
+    pid = fork();  
+    if( pid == 0 )
+    {
+        pid_info("B before");
 
+        cout << " B" << endl; 
+        cout.flush();
+        sleep(10);
+
+        pid_info("B after");
+
+        return 0;
+    }
+
+    pid = fork();
+    if ( pid == 0 )
+    {
+        cout << " C " << endl;
+        cout.flush();
+    }
 }
+/*
+ A 
+ B
+ C 
+
+ A 
+ B
+ C 
+ C 
+
+*/
