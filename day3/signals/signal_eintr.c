239af6690d60671b6c29133cfb13756096846b23
Marcin Klimek <git@marcinklimek.pl>
2014-02-26 09:47:07 +0100

    sig

--- day3/signals/signal_eintr.c
+++ day3/signals/signal_eintr.c
@@ -15,10 +15,12 @@ int main(int argc, char const *argv[])
     static struct sigaction action;
     char buf[512];
     int nread;
-    action.sa_flags = SA_RESTART;
+    //action.sa_flags = SA_RESTART;
     action.sa_handler = catch_int;
     sigaction( SIGUSR1, &action, NULL);
     //signal( SIGUSR1, catch_int);
+
+    printf("PID: %d\n", getpid());
 
     nread = read (fileno(stdin), buf, sizeof(buf));
     if (nread < 0) 
