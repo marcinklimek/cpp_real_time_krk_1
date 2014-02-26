e06afe4e70c150eeef65377d2b523c187029ed67
Marcin Klimek <git@marcinklimek.pl>
2014-02-26 09:47:44 +0100

    maski

---
+++ day3/signals/sig_start.c
@@ -0,0 +1,51 @@
+#include <signal.h>
+#include <stdio.h>
+#include "../inc/misc_functions.h"
+
+void catch_int(int signum)
+{
+    printf("Handler %d\n", signum);
+    _exit(1);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    static struct sigaction action;
+    static struct sigaction old_action;
+    sigset_t set1, set2;
+
+
+    action.sa_handler = catch_int;
+    sigaction( SIGINT, &action, &old_action);
+
+
+    sigfillset( &set1 );
+
+    sigfillset( &set2 );
+    sigdelset( &set2, SIGINT );   
+
+    // sekcja krytyczna
+    sigprocmask( SIG_SETMASK, &set1, NULL);
+    
+    printf("Sekcja krytyczna\n");
+
+    raise( SIGINT );
+
+    printf("Po sekcji krytycznej\n");
+    sigprocmask( SIG_SETMASK, &set2, NULL);
+    //raise( SIGINT );
+
+    printf("KONIEC\n");
+
+
+    for( i=0; i<30; i++)
+    {
+        printf(".");
+        fflush(stdout);
+        sleep(1);
+    }
+   
+    return 0;
+}
