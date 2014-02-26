5f74645d4973bf814c8fb4deba1f4bf4d2472b7e
Marcin Klimek <git@marcinklimek.pl>
2014-02-26 09:37:12 +0100

    intr

--- day3/signals/sig1.c
+++ day3/signals/sig1.c
@@ -1,13 +1,25 @@
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include "../inc/misc_functions.h"
 
+void catch_int(int signum)
+{
+    printf("Handler %d\n", signum);
+}
 
 int main(int argc, char const *argv[])
 {
-    static struct sigaction action;
-    static struct sigaction old_action;
+    //static struct sigaction action;
+    //static struct sigaction old_action;
     int i;
     
+    //action.sa_handler = catch_int;
+    //sigaction( SIGINT, &action, NULL );
+
+    signal( SIGINT, catch_int);
 
     for( i=0; i<30; i++)
     {
---
+++ day3/signals/signal_eintr.c
@@ -0,0 +1,34 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+void catch_int(int signum)
+{
+    printf("Handler %d\n", signum);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    static struct sigaction action;
+    char buf[512];
+    int nread;
+    action.sa_flags = SA_RESTART;
+    action.sa_handler = catch_int;
+    sigaction( SIGUSR1, &action, NULL);
+    //signal( SIGUSR1, catch_int);
+
+    nread = read (fileno(stdin), buf, sizeof(buf));
+    if (nread < 0) 
+    {
+        printf( "nred < 0, %s\n", strerror( errno ) );
+        return 1;
+    }
+
+    printf("Num read = %d -> %s\n", nread, buf);
+
+    return 0;
+}
+
